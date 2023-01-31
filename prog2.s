d6d4777347b3852753f1ba0ed64dc663076e9607
Aaryan Tiwary <75285979+atiwaryandroid@users.noreply.github.com>
2023-01-30 23:25:07 -0800

    Update prog2.s

--- prog2.s
+++ prog2.s
@@ -4,5 +4,7 @@ SHIFT_LEFT_I R3 2 //R3 << 3, R3 now stores 24
 ADDI R3 3//R3 = R3 + 4
 ADDI R3 1 //R3 = R3 + 2, now R3 = 30
 //Now, to not use any more registers for storing upper limit (59), I am going to store 60 in uppper part of memory  from which I can load when required
-MOV R2 R3
-SHIFT_LEFT R3 1
+MOV R0 R3
+SHIFT_LEFT R0 1 //Now, R0 = 60
+MOVI R1 0 //R1 = 0
+STORE_TOP_BYTE R1 0 //mem[top] = R0 = 60
