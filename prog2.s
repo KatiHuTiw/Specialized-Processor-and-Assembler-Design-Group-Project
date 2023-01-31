c2919375396165609623b96e7b623d70def067ef
Aaryan Tiwary <75285979+atiwaryandroid@users.noreply.github.com>
2023-01-31 00:54:32 -0800

    Update prog2.s

--- prog2.s
+++ prog2.s
@@ -4,11 +4,10 @@ SHIFT_LEFT_I R3 2 //R3 << 3, R3 now stores 24
 ADDI R3 3//R3 = R3 + 4
 ADDI R3 1 //R3 = R3 + 2, now R3 = 30, R3 will be used as index in this program
 //Now, to not use any more registers for storing upper limit (59), I am going to store 60 in uppper part of memory  from which I can load when required
-MOVI R1 0 //R1 = 0
 MOV R0 R3 //R0 = 30
-STORE_TOP_BYTE R1 1 //mem[top - 1] = 30 (30 is stored because it will be needed inside for loop again and again to get lower memory address)
+STORE_TOP_BYTE_I 1 //mem[top - 1] = 30 (30 is stored because it will be needed inside for loop again and again to get lower memory address)
 SHIFT_LEFT R0 1 //Now, R0 = 60
-STORE_TOP_BYTE R1 0 //mem[top] = R0 = 60
+STORE_TOP_BYTE_I 0 //mem[top] = R0 = 60
 NOP
 NOP //The place where loop starts, relative jumps will be used to get here, loop uses R3 as index R3 goes from 30 to 58 in jumps of 2
 NOP //Code executed inside loop starts below this NOP
@@ -17,6 +16,9 @@ MOV R2 R0 //R2 = R0, now R2 is D11 D10 D9 D8 D7 D6 D5 P8
 SHIFT_RIGHT_I R2 0 //R2 = R2 >> 1, R2 is 0 D11 D10 D9 D8 D7 D6 D5
 SHIFT_LEFT_I R2 0 //R2 is D11 D10 D9 D8 D7 D6 D5 0
 XOR_REG R2 R1 //R1's least significant bit = D5 ^ D6 ^ D7 ^ D8 ^ D9 ^ D10 ^ D11
+MOV R2 R0 //R2 is D11 D10 D9 D8 D7 D6 D5 P8
+MOV R0 R1 //R0 = 0 0 0 0 0 0 0 Q8
+STORE_TOP_BYTE_I 2 //mem[top - 2] = 0 0 0 0 0 0 0 Q8
 NOP //Code executed inside loop ends above this NOP
 MOV R1 0 //R1 = 0
 LOAD_TOP_BYTE R1 0 //R0 = mem[top] = 60
