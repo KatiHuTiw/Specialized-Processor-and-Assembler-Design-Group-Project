2d466dae36c74853b765adfce6daaffea5b377db
Aaryan Tiwary <75285979+atiwaryandroid@users.noreply.github.com>
2023-01-30 23:13:15 -0800

    Update prog2.s

--- prog2.s
+++ prog2.s
@@ -1,5 +1,8 @@
 //I am supposed to read memory from address 30-59, so I am going to keep track of memory address in register R3, starting from 30
-MOVI R3 11
+MOVI R3 3
 SHIFT_LEFT_I R3 2 //R3 << 3, R3 now stores 24
-ADDI R3 11 //R3 = R3 + 4
-ADDI R3 01 //R3 = R3 + 2, now R3 = 30
+ADDI R3 3//R3 = R3 + 4
+ADDI R3 1 //R3 = R3 + 2, now R3 = 30
+//Now, to not use any more registers for storing upper limit (59), I am going to store 60 in uppper part of memory  from which I can load when required
+MOV R2 R3
+SHIFT_LEFT R3 1
