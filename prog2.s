11a3e99f96c43c3114a161f00a11449541c6f8f5
Aaryan Tiwary <75285979+atiwaryandroid@users.noreply.github.com>
2023-01-31 00:02:00 -0800

    Update prog2.s

--- prog2.s
+++ prog2.s
@@ -2,11 +2,12 @@
 MOVI R3 3
 SHIFT_LEFT_I R3 2 //R3 << 3, R3 now stores 24
 ADDI R3 3//R3 = R3 + 4
-ADDI R3 1 //R3 = R3 + 2, now R3 = 30
+ADDI R3 1 //R3 = R3 + 2, now R3 = 30, R3 will be used as index in this program
 //Now, to not use any more registers for storing upper limit (59), I am going to store 60 in uppper part of memory  from which I can load when required
-MOV R0 R3
-SHIFT_LEFT R0 1 //Now, R0 = 60
 MOVI R1 0 //R1 = 0
+MOV R0 R3 //R0 = 30
+STORE_TOP_BYTE R1 1 //mem[top - 1] = 30 (30 is stored because it will be needed inside for loop again and again to get lower memory address)
+SHIFT_LEFT R0 1 //Now, R0 = 60
 STORE_TOP_BYTE R1 0 //mem[top] = R0 = 60
 NOP
 NOP //The place where loop starts, relative jumps will be used to get here
