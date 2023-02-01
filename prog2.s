c920fce7d28e8c7f02a43e0c91def7836f437fb7
Aaryan Tiwary <75285979+atiwaryandroid@users.noreply.github.com>
2023-02-01 11:35:25 -0800

    Update prog2.s

--- prog2.s
+++ prog2.s
@@ -100,8 +100,9 @@ AND R1 R0 //R1 = 0 0 0 0 0 0 0 P0
 MOV R2 3
 LOAD_TOP_BYTE R2 3 //R0 = mem[top - 6] = 0 0 0 0 0 0 0 Q0
 Error: BEQ (some place (not decided yet) where S0 and S1 and S2 and S4 and S8 will be computed and checked)
-// Code to execute if P0 and Q0 are not the same, so the case in which there is a 1-bit error
+// Start of Code to execute if P0 and Q0 are not the same, so the case in which there is a 1-bit error
 //To be implemented
+// End of code to execute if P0 and Q0 are not the same, so the case in which there is a 1-bit error
 Error: B (unconditional branch to skip over code execution that occurs when P0 and Q0 are equal)
 // The above BEQ leads here - Code to execute if P0 and Q0 are the same
 LOAD_BYTE R3 0 //R0 = mem[R3]
