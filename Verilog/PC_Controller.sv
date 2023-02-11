49cbf59898226bb69c202062263b65f9f4e4111f
jgr1024 <66024759+jgr1024@users.noreply.github.com>
2023-02-11 12:35:46 -0800

    Added sign extension for branching

--- Verilog/PC_Controller.sv
+++ Verilog/PC_Controller.sv
@@ -3,22 +3,22 @@ module PC_Controller #(parameter D=12)(
 	input       [ 3:0] pc_ctrl_input,	   // 4 bits = 16 different LUT value
 	output logic[D-1:0] target
 );
-logic [7:0] offset;
+logic [D-1:0] offset;
 
 assign target = offset;
 
   always_comb begin
 	if(immOrLUT) begin
 		case(pc_ctrl_input)
-			// add signed bits to target
-				'b0000: offset = 'b11111011;   // go back 5 spaces
-				'b0001: offset = 'b00010100;   // go ahead 20 spaces
-				'b0010: offset = 'b11111111;   // go back 1 space   1111_1111_1111
+			// Send signed bits. ALL VALUES IN THE LUT ARE SIGNED!!!!
+				'b0000: offset = 'b111111111011;   // go back 5 spaces
+				'b0001: offset = 'b000000010100;   // go ahead 20 spaces
+				'b0010: offset = 'b111111111111;   // go back 1 space   1111_1111_1111
 				default: offset = 'b0;  // hold PC  
 		endcase
 	end
 	else begin
-		offset = pc_ctrl_input;
+		offset = {{D-4{pc_ctrl_input[3]}},pc_ctrl_input};// sign extend and send
 	end
 	
   end
