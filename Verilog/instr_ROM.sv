75d289328c8b5cf94a2f57c31ca176bd2a13dcac
jgr1024 <66024759+jgr1024@users.noreply.github.com>
2023-02-17 20:50:42 -0800

    Added instructions on how to use

--- Verilog/instr_ROM.sv
+++ Verilog/instr_ROM.sv
@@ -7,7 +7,9 @@ module instr_ROM #(parameter D=12)(
 
   logic[8:0] core[2**D];
   initial							    // load the program
-    $readmemb("machine_code.txt",core);
+    // Please put the global path for the machine code (C:/....) quartus needs the machine code with the other verilog files and 
+    // the simulator checks the simulation/modelsim(quest) folder for the machine code. Specifying global path works on both instances.
+    //$readmemb("machine_code.txt",core);
 
   always_comb  mach_code = core[prog_ctr];
 
